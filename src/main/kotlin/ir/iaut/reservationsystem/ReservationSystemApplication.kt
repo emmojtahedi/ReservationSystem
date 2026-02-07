11e6e5fefdec8804b63139b6ae9df187ea99949b
itmcoitcs <itmcoitcs@gmail.com>
2026-02-07 16:26:17 +0330

    init project

--- src/main/kotlin/ir/iaut/reservationsystem/ReservationSystemApplication.kt
+++ src/main/kotlin/ir/iaut/reservationsystem/ReservationSystemApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ReservationSystemApplication
+open class ReservationSystemApplication
 
 fun main(args: Array<String>) {
     runApplication<ReservationSystemApplication>(*args)
